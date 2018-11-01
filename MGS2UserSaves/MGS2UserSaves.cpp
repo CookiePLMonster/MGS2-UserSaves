d8e6aae898e3eaa5683e05573137839a4964562f
Silent <zdanio95@gmail.com>
2018-11-02 00:07:46 +0100

    Initial commit

---
+++ MGS2UserSaves/MGS2UserSaves.cpp
@@ -0,0 +1,23 @@
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#define WINVER 0x0502
+#define _WIN32_WINNT 0x0502
+
+#include <windows.h>
+
+static void InstallHooks()
+{
+
+}
+
+
+extern "C"
+{
+	static LONG InitCount = 0;
+	__declspec(dllexport) void InitializeASI()
+	{
+		if ( _InterlockedCompareExchange( &InitCount, 1, 0 ) != 0 ) return;
+		InstallHooks();
+	}
+}
